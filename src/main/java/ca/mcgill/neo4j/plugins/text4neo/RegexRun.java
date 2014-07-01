7993d77d5d2c2e90f1265b899132d7c843a17449
Mahmood Shafeie Zargar <mahmood@gmail.com>
2014-07-01 04:20:56 -0400

    Initial commit + formatting

--- src/main/java/ca/mcgill/neo4j/plugins/text4neo/RegexRun.java
+++ src/main/java/ca/mcgill/neo4j/plugins/text4neo/RegexRun.java
@@ -25,47 +25,43 @@ import org.neo4j.tooling.GlobalGraphOperations;
 
 import java.util.ArrayList;
 
-@Description( "An extension to the Neo4j Server that can run regex on node properties and return or save the results." )
-public class RegexRun extends ServerPlugin
-{
-    @Name( "rx_replace" )
-    @Description( "Detects patterns in string properties, replaces them with a substitute string and returns or saves the resulting strings." )
-    @PluginTarget( GraphDatabaseService.class )
-    public Iterable<String> rxReplace (
+@Description("An extension to the Neo4j Server that can run regex on node properties and return or save the results.")
+public class RegexRun extends ServerPlugin {
+    @Name("rx_replace")
+    @Description("Detects patterns in string properties, replaces them with a substitute string and returns or saves the resulting strings.")
+    @PluginTarget(GraphDatabaseService.class)
+    public Iterable<String> rxReplace(
             @Source GraphDatabaseService graphDb,
-            @Description( "The label by which nodes should be filtered." )
-            @Parameter( name = "label", optional = false ) String label,
-            @Description( "The property on which you would like to run the regexp statement." )
-            @Parameter( name = "property", optional = false ) String inProperty,
-            @Description( "The regex statement you would like to run (Java string formatted)." )
-            @Parameter( name = "statement", optional = false ) String statement,
-            @Description( "The string with which you want to replace the pattern." )
-            @Parameter( name = "replacement", optional = false ) String replace,
-            @Description( "The node to which you would like to write the results. By default this parameter is empty and the data is returned instead of being saved." )
-            @Parameter( name = "output", optional = true ) String outProperty,
-            @Description( "Whether all the instances of the pattern should be replaced. Defaults to true." )
-            @Parameter( name = "replaceall", optional = true ) Boolean all
-    )
-    {
+            @Description("The label by which nodes should be filtered.")
+            @Parameter(name = "label", optional = false) String label,
+            @Description("The property on which you would like to run the regexp statement.")
+            @Parameter(name = "property", optional = false) String inProperty,
+            @Description("The regex statement you would like to run (Java string formatted).")
+            @Parameter(name = "statement", optional = false) String statement,
+            @Description("The string with which you want to replace the pattern.")
+            @Parameter(name = "replacement", optional = false) String replace,
+            @Description("The node to which you would like to write the results. By default this parameter is empty and the data is returned instead of being saved.")
+            @Parameter(name = "output", optional = true) String outProperty,
+            @Description("Whether all the instances of the pattern should be replaced. Defaults to true.")
+            @Parameter(name = "replaceall", optional = true) Boolean all
+    ) {
         ArrayList<String> results = new ArrayList<>();
         Label searchLabel = DynamicLabel.label(label);
         int count = 0;
 
-        try (Transaction tx = graphDb.beginTx())
-        {
-           for ( Node node : GlobalGraphOperations.at( graphDb ).getAllNodesWithLabel(searchLabel) )
-            {
+        try (Transaction tx = graphDb.beginTx()) {
+            for (Node node : GlobalGraphOperations.at(graphDb).getAllNodesWithLabel(searchLabel)) {
                 String result;
 
-                if (all == null || all == true) {
-                    result = node.getProperty(inProperty, "").toString().replaceAll(statement,replace);
+                if (all == null || all) {
+                    result = node.getProperty(inProperty, "").toString().replaceAll(statement, replace);
                 } else {
-                    result = node.getProperty(inProperty, "").toString().replaceFirst(statement,replace);
+                    result = node.getProperty(inProperty, "").toString().replaceFirst(statement, replace);
                 }
 
                 if (!result.isEmpty()) {
                     if (!(outProperty == null) && !(outProperty.isEmpty())) {
-                        node.setProperty(outProperty,result);
+                        node.setProperty(outProperty, result);
                         count++;
                     } else {
                         results.add(result);
